4104ebd74d3f206f4722bf9bfcb53ca6528789c1
benjamin.jaume <47286801+BenjaminJaume@users.noreply.github.com>
2022-09-22 16:01:14 -0600

    Print error

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -60,7 +60,7 @@ module.exports = {
           });
       })
       .catch((error) => {
-        // console.log(error);
+        console.log(error);
         res.status(400).send({ message: error.message });
       });
   },

a3c4f5c6dfc6f6eacf4d74d04c8b26acfda70480
benjamin.jaume <47286801+BenjaminJaume@users.noreply.github.com>
2022-09-24 16:10:42 -0600

    Fixing rides to confirm bug

--- controllers/ride.controller.js
+++ controllers/ride.controller.js
@@ -911,7 +911,6 @@ module.exports = {
                 // A feedback need to be given
                 return RideFeedback.findOne({
                   where: {
-                    UserId: user.id,
                     RideId: ride.id,
                     BookingId: booking.id,
                   },

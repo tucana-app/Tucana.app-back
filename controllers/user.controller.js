f61c3099c825ebd8bc0d90deb100852ee3682f3e
benjamin.jaume <47286801+BenjaminJaume@users.noreply.github.com>
2023-02-22 10:19:35 -0600

    Add user to mailchimp when sign up

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,11 @@
 const path = require("path");
 const fileName = path.basename(__filename);
+const client = require("@mailchimp/mailchimp_marketing");
+
+client.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_SERVER,
+});
 
 const db = require("../models");
 const config = require("../config/user.config");
@@ -36,6 +42,21 @@ module.exports = {
   signup(req, res) {
     const { firstName, lastName, email, phoneNumber, username, password } =
       req.body.values;
+
+    (async () => {
+      const response = await client.lists
+        .addListMember(process.env.MAILCHIMP_LIST_ID, {
+          email_address: email,
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName,
+          },
+          status: "subscribed",
+        })
+        .catch((error) => {
+          consoleError(fileName, arguments.callee.name, Error().stack, error);
+        });
+    })();
 
     return User.create({
       firstName,
